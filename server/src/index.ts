b43a56c2c0d51e10e2217b71fcfb97f95360bafd
Hasip Timurtaş <hasip.timurtas@gmail.com>
2021-03-01 17:40:18 +0000

    User interface added, register done. password bcrypt implemented.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,13 @@ import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
-import User from './model/User';
+import User from './models/User';
+import { UserInterface } from './interfaces/UserInterface';
 
 dotenv.config();
 
 const PORT = 4000;
+const LocalStrategy = passportLocal.Strategy;
 
 mongoose.connect(`${process.env.DBLINK}`, {
     useCreateIndex: true,
@@ -35,24 +37,68 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Password
+passport.use(new LocalStrategy((username, password, done) => {
+    User.findOne({ username }, (err: Error, user: UserInterface) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) throw err;
+            if (result === true) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    });
+}));
+
+passport.serializeUser((user: any, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser((id: string, cb) => {
+    User.findOne({ _id: id }, (err: Error, user: UserInterface) => {
+        const userInfo = {
+            username: user.username,
+            isAdmin: user.isAdmin
+        };
+        cb(err, userInfo);
+    });
+});
+
+
 // Routes
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body
-    console.log(username, password);
+app.post('/register', async (req: Request, res: Response) => {
     try {
-        const hashedPassword: string = await bcrypt.hash(password, 10);
-        const newUser = new User({
-            username,
-            passport: hashedPassword
+        const { username, password } = req?.body
+
+        if (!username || !password || typeof username !== "string" || typeof password !== "string") {
+            res.send("Improper Values");
+            return;
+        }
+
+        User.findOne({ username }, async (err: Error, doc: UserInterface) => {
+            if (err) throw err;
+            if (doc) res.send("User Already Exists");
+            if (!doc) {
+                const hashedPassword: string = await bcrypt.hash(password, 10);
+                const newUser = new User({
+                    username,
+                    password: hashedPassword
+                });
+
+                await newUser.save();
+                res.send("Success");
+            }
         });
 
-        await newUser.save();
-        res.send("Success");
     } catch (err) {
         console.log(err);
         res.send("Error");
     }
-
 });
+
+
 
 app.listen(PORT, () => console.log(`Server listenin on port : ${PORT}`));
