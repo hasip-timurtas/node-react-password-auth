7a0eca1fc68e533138bc2225a0f810709a25180d
Hasip Timurtaş <hasip.timurtas@gmail.com>
2021-03-01 21:37:10 +0000

    client - navbar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from "./Pages/HomePage";
+import AdminPage from "./Pages/AdminPage";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
 
 export default function App() {
     return (
-        <div>App</div>
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route path='/' exact component={HomePage}></Route>
+                <Route path='/admin' component={AdminPage}></Route>
+                <Route path='/login' component={Login}></Route>
+                <Route path='/profile' component={Profile}></Route>
+            </Switch>
+        </Router>
     );
 }
