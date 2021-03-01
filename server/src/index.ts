2ddcfcdf1d1863cc198c6ca28c8b18f05927fc57
Hasip Timurtaş <hasip.timurtas@gmail.com>
2021-03-01 15:55:55 +0000

    server - mongodb connection and  middlewares added

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import mongoose, { Error } from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+
+
+mongoose.connect('mongodb+srv://@cluster0.q9g9s.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err: Error) => {
+    if (err) throw err;
+    console.log('MongoDb Connected');
+});
+
+// MiddleWare
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(session({
+    secret: "mysecretcode",
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
