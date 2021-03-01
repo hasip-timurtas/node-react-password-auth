61a464a320977608b074dd2e1549c0ddf414ebec
Hasip Timurtaş <hasip.timurtas@gmail.com>
2021-03-01 21:42:57 +0000

    client - TailwindCSS added, anv links added

--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    return (<div>NavBar</div>);
+    return (
+        <div className="nav-container">
+            <Link to="/logout">Logout</Link>
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/admin">Admin</Link>
+        </div>
+    );
 }
